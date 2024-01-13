d50701eb6083a60e9f5805b129b7a5e01a0017ed
IvannDegtyarenko <gojo2351@gmail.com>
2024-01-13 04:56:43 +0300

    Случайное число в функцию, логич. разд. и остальные недочёты

--- laboratory-task-3/main.cpp
+++ laboratory-task-3/main.cpp
@@ -22,6 +22,11 @@ bool checkStream()
 	return true;
 }
 
+int getRandomNumber(const int64_t& leftBound, const int64_t& rightBound)
+{
+	return (rand() % (rightBound - leftBound + 1) + leftBound);
+}
+
 int getArraySize()
 {
 	int64_t arraySize = 0;
@@ -31,6 +36,7 @@ int getArraySize()
 		std::cout << "Enter a number instead of the letter: ";
 		std::cin >> arraySize;
 	}
+
 	while (arraySize <= 0 || arraySize >= 255) {
 		std::cout << "Wrong value! Enter right: ";
 		std::cin >> arraySize;
@@ -47,6 +53,7 @@ void fillArrayRand(int32_t* arr, const size_t& arraySize)
 	int64_t min = 0;
 	int64_t max = 0;
 	std::cout << "\nEnter the boundaries for finding random numbers: ";
+
 	while (1) {
 		std::cin >> min;
 		if (checkStream()) {
@@ -59,25 +66,28 @@ void fillArrayRand(int32_t* arr, const size_t& arraySize)
 			break;
 		}
 	}
+
 	if (max < min) {
 		int64_t temp = min;
 		min = max;
 		max = temp;
 	}
 	for (size_t i = 0; i < arraySize; ++i) {
-		arr[i] = rand() % (max - min + 1) + min;
+		arr[i] = getRandomNumber(min, max);
 	}
 }
 
-void fillArrChoice(int32_t* arr, const size_t& arraySize)
+uint32_t getInputType()
 {
 	std::cout << "Choose the type of input: 1-automatically, 2-manually: ";
-	uint64_t inputType = 0;
+	uint32_t inputType = 0;
 	std::cin >> inputType;
+
 	while (!checkStream()) {
 		std::cout << "Enter a number instead of the letter: ";
 		std::cin >> inputType;
 	}
+
 	while (inputType <= 0 || inputType > 2) {
 		std::cout << "Wrong value! Enter right: ";
 		std::cin >> inputType;
@@ -85,6 +95,12 @@ void fillArrChoice(int32_t* arr, const size_t& arraySize)
 			continue;
 		}
 	}
+
+	return inputType;
+}
+
+void switchInputType(int32_t* arr, const size_t& arraySize, const uint32_t& inputType)
+{
 	switch (inputType) {
 	case 1: {
 		fillArrayRand(arr, arraySize);
@@ -104,6 +120,12 @@ void fillArrChoice(int32_t* arr, const size_t& arraySize)
 	}
 }
 
+void fillArrChoice(int32_t* arr, const size_t& arraySize)
+{
+	uint32_t inputType = getInputType();
+	switchInputType(arr, arraySize, inputType);
+}
+
 void printArray(int32_t* arr, const size_t& arraySize)
 {
 	for (size_t i = 0; i < arraySize; ++i) {
@@ -120,7 +142,7 @@ int sumOfTheElements(int32_t* arr, const size_t& arraySize)
 	return sum;
 }
 
-int firstPositive(int32_t* arr, const size_t& arraySize)
+int firstPosDigInd(int32_t* arr, const size_t& arraySize)
 {
 	int32_t firstPositiveDigit = -1;
 	for (size_t i = 0; i < arraySize; ++i) {
@@ -137,15 +159,16 @@ int firstPositive(int32_t* arr, const size_t& arraySize)
 	}
 }
 
-int lastPositive(int32_t* arr, const size_t& arraySize)
+int lastPosDigInd(int32_t* arr, const size_t& arraySize)
 {
-	int32_t lastPositiveDigit = firstPositive(arr, arraySize);
-	for (size_t i = firstPositive(arr, arraySize); i < arraySize; ++i) {
+	int32_t lastPositiveDigit = firstPosDigInd(arr, arraySize);
+	for (size_t i = firstPosDigInd(arr, arraySize); i < arraySize; ++i) {
 		if (arr[i] > 0 && i > lastPositiveDigit) {
 			lastPositiveDigit = i;
 		}
 	}
-	if (lastPositiveDigit == firstPositive(arr, arraySize)) {
+
+	if (lastPositiveDigit == firstPosDigInd(arr, arraySize)) {
 		return -1;
 	}
 	else {
@@ -153,25 +176,28 @@ int lastPositive(int32_t* arr, const size_t& arraySize)
 	}
 }
 
-void elemMultiplication(int32_t* arr, int32_t firstPositiveDigit, int32_t lastPositiveDigit, int64_t& multiplication)
+int32_t elemMultiplication(int32_t* arr, int32_t firstPositiveDigitIndx, int32_t lastPositiveDigitIndx)
 {
-	if (firstPositiveDigit == -1 || lastPositiveDigit == -1) {
+	int32_t multiplication = 1;
+	if (firstPositiveDigitIndx == -1 || lastPositiveDigitIndx == -1) {
 		throw std::exception("\nCan't provide multiplication");
 	}
-	for (size_t i = firstPositiveDigit; i <= lastPositiveDigit; ++i) {
+	for (size_t i = firstPositiveDigitIndx; i <= lastPositiveDigitIndx; ++i) {
 		multiplication *= arr[i];
 	}
+	return multiplication;
 }
 
-void arrayMinimizing(int32_t* arr, const size_t& arraySize)
+void changeArray(int32_t* arr, const size_t& arraySize)
 {
-	std::cout << "\nEnter your number for minimizing rule: ";
+	std::cout << "\nEnter your number for changing rule: ";
 	int32_t userNumber = 0;
 	std::cin >> userNumber;
 	while (!checkStream()) {
 		std::cout << "Enter a number instead of the letter: ";
 		std::cin >> userNumber;
 	}
+
 	while (userNumber < 0) {
 		std::cout << "Wrong value! Enter right: ";
 		std::cin >> userNumber;
@@ -179,6 +205,7 @@ void arrayMinimizing(int32_t* arr, const size_t& arraySize)
 			continue;
 		}
 	}
+
 	for (size_t i = 0; i < arraySize; ++i) {
 		if (abs(arr[i]) > userNumber) {
 			arr[i] = 0;
@@ -205,27 +232,27 @@ int main()
 		std::cout << "Your array: ";
 		printArray(arr, arraySize);
 		std::cout << "\nThe sum of the elements of the array with odd numbers = " << sumOfTheElements(arr, arraySize);
+
 		try {
-			int32_t firstPositiveDigit = firstPositive(arr, arraySize);
-			int32_t lastPositiveDigit = lastPositive(arr, arraySize);
-			int64_t multiplication = 1;
-			elemMultiplication(arr, firstPositiveDigit, lastPositiveDigit, multiplication);
-			std::cout << "\nMultiplication of elements between the first and last positive number = " << multiplication;
+			int32_t firstPositiveDigitIndx = firstPosDigInd(arr, arraySize);
+			int32_t lastPositiveDigitIndx = lastPosDigInd(arr, arraySize);
+			std::cout << "\nMultiplication of elements between the first and last positive number = ";
+			std::cout << elemMultiplication(arr, firstPositiveDigitIndx, lastPositiveDigitIndx);
 		}
 		catch (std::exception& error) {
 			std::cout << error.what();
 		}
-		std::cout << "\nMinimizing in progress . . ." << '\n';
-		system("pause");
-		system("cls");
-		std::cout << "Your array before minimizing: ";
+
+		std::cout << "\nChanging in progress . . ." << '\n';
+
+		std::cout << "Your array before changing: ";
 		printArray(arr, arraySize);
-		arrayMinimizing(arr, arraySize);
-		std::cout << "\nYour array after minimizing: ";
+		changeArray(arr, arraySize);
+		std::cout << "\nYour array after changing: ";
 		printArray(arr, arraySize);
 	}
 	catch (std::exception& error) {
-		std::cout << error.what();
+		std::cerr << error.what();
 		return -1;
 	}
 	return 0;
