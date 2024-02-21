54a6b28dc0d503cf7da1f73788787f997f736e87
IvannDegtyarenko <gojo2351@gmail.com>
2024-02-21 19:25:13 +0300

    Добавил класс myString

---
+++ laboratory-task-10/src/main/main.cpp
@@ -0,0 +1,41 @@
+#include "../fraction/Fraction.hpp"
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	try {
+		MyString a("fer");
+		std::cout << a;
+		a = "red";
+		a += "der";
+		a.toUpperCase();
+		std::cout << a;
+		a.toLowerCase();
+		std::cout << a;
+		std::cout << a[0] << '\n';
+		a.printString();
+		std::cin >> a;
+		a.toUpperCase();
+		std::cout << a;
+		MyString c;
+		std::cout << c.getSize();
+		MyString b("FER");
+		if (a == b) {
+			std::cout << "Strings are equal\n";
+		}
+		else {
+			std::cout << "Strings are not equal\n";
+		}
+	}
+	catch (std::invalid_argument& e) {
+		std::cerr << e.what();
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what();
+	}
+	catch (std::out_of_range& e) {
+		std::cerr << e.what();
+	}
+	return 0;
+}
