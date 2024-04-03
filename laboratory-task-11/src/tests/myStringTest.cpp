d27cfe19e647a94521290456c925d4c05a74eacd
IvannDegtyarenko <gojo2351@gmail.com>
2024-04-03 20:52:41 +0300

    Добавил main в конце файла тестов

--- laboratory-task-11/src/tests/myStringTest.cpp
+++ laboratory-task-11/src/tests/myStringTest.cpp
@@ -107,3 +107,8 @@ TEST(testBoolOperators, testLess)
     MyString anotherCheckString("abb");
     EXPECT_FALSE(testStr < anotherCheckString);
 }
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
