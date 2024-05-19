c412660bfd6d36ebf840db624b6b6332b292adb9
IvannDegtyarenko <gojo2351@gmail.com>
2024-05-19 10:48:14 +0300

    Добавил лабораторную работу №5

---
+++ laboratory-task-14/src/Wagon/wagon.cpp
@@ -0,0 +1,38 @@
+#include "wagon.hpp"
+
+Wagon::Wagon(std::string initColor) : color(initColor)
+{}
+
+Wagon::Wagon(const Wagon& src) : color(src.color)
+{}
+
+Wagon& Wagon::operator=(const Wagon& src)
+{
+    if (this != &src) {
+        this->color = src.color;
+    }
+    return *this;
+}
+
+Wagon& Wagon::operator=(std::string src)
+{
+    this->color = src;
+    return *this;
+}
+
+std::string Wagon::getColor() const
+{
+    std::string safe = this->color;
+    return safe;
+}
+
+void Wagon::setColor(std::string src)
+{
+    this->color = src;
+}
+
+std::ostream& operator<<(std::ostream& out, const Wagon& src)
+{
+    out << "Wagon color:" << src.color;
+    return out;
+}
