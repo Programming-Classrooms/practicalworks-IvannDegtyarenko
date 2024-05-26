f31512a6b9e6609465d23e6e7bae44a2e2512770
IvannDegtyarenko <gojo2351@gmail.com>
2024-05-26 16:50:44 +0300

    Добавил пустые строки в конце файлов

--- laboratory-task-12/src/classFolder/Matrix.hpp
+++ laboratory-task-12/src/classFolder/Matrix.hpp
@@ -46,4 +46,4 @@ public:
 	friend std::ostream& operator<<(std::ostream&, const Matrix&);
 	friend std::istream& operator>>(std::istream&, const Matrix&);
 };
-#endif // CLASSMATRIX_H
+#endif // CLASSMATRIX_HPP
